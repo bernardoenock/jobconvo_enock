07d77b2d316d655d82cb2a6fbd0e52e8439796b7
Enock <bernardo.enock@vileve.com.br>
2025-08-19 17:39:06 -0300

    create models for accounts

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as gl
 
-# Create your models here.
+class User(AbstractUser):
+  username = None
+  email = models.EmailField(gl('email address'), unique=True)
+
+  USERNAME_FIELD = 'email'
+  REQUIRED_FIELDS = []
+
+class Company(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='company')
+  name = models.CharField(max_length=255)
+
+  def __str__(self):
+    return self.name
+
+class Candidate(models.Model):
+  class Education(models.IntegerChoices):
+     FUNDAMENTAL = 1, 'Ensino fundamental'
+     MEDIO = 2, 'Ensino médio'
+     TECNOLOGO = 3, 'Tecnólogo'
+     SUPERIOR = 4, 'Ensino superior'
+     POS_MBA_MESTRADO = 5, 'Pós / MBA / Mestrado'
+     DOUTORADO = 6, 'Doutorado'
+  
+  user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='candidate')
+  last_education = models.IntegerField(choices=Education.choices, default=Education.MEDIO)
+  experience = models.TextField(blank=True)
+
+  def __str__(self):
+    return self.user.email
