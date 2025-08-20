15e5d18b34e4a7f2b1364ca4fa224093cf36b10f
Enock <bernardo.enock@vileve.com.br>
2025-08-20 17:40:11 -0300

    fix models

--- accounts/models.py
+++ accounts/models.py
@@ -2,13 +2,16 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as gl
 from .managers import UserManager
+
 class User(AbstractUser):
   username = None
   email = models.EmailField(gl('email address'), unique=True)
+  
+  is_company = models.BooleanField(default=False)
+  is_candidate = models.BooleanField(default=False)
 
   USERNAME_FIELD = 'email'
   REQUIRED_FIELDS = []
-
 
   objects = UserManager()
 
