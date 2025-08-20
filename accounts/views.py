0e8ab11131d9a5dc4c7c8d9dadb4ed2d9144e53d
Enock <bernardo.enock@vileve.com.br>
2025-08-20 11:54:24 -0300

    add views for accounts and jobs

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import CompanySignUpForm, CandidateSignUpForm
 
-# Create your views here.
+def company_signup(request):
+  if request.method == 'POST':
+    form = CompanySignUpForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('jobs:job_list')
+  
+  else:
+    form = CompanySignUpForm()
+  
+  return render(request, 'accounts/signup_form.html', {'form': form})
+
+
+def candidate_signup(request):
+  if request.method == 'POST':
+    form = CandidateSignUpForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('jobs:job_list')
+  
+  else:
+    form = CandidateSignUpForm()
+  
+  return render(request, 'accounts/signup_form.html', {'form': form})
--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,117 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.db.models import Count
+from django.db.models.functions import TruncMonth
+from django.http import JsonResponse, Http404
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
-# Create your views here.
+from accounts.models import Company, Candidate
+from .models import Job, Application
+from .forms import JobForm, ApplicationForm
+
+class CompanyRequiredMixin(UserPassesTestMixin):
+  def test_func(self):
+    return hasattr(self.request.user, 'company')
+  
+class CandidateRequiredMixin(UserPassesTestMixin):
+  def test_func(self):
+    return hasattr(self.request.user, 'candidate')
+  
+class JobListView(ListView):
+  model = Job
+  template_name = 'jobs/job_list.html'
+  context_object_name = 'jobs'
+
+  def get_queryset(self):
+    return Job.objects.all().annotate(app_count=Count('applications')).order_by('-created_at')
+  
+class JobDetailView(DetailView):
+  model = Job
+  template_name = 'jobs/job_detail.html'
+  context_object_name = 'job'
+
+class JobCreateView(LoginRequiredMixin, CompanyRequiredMixin, CreateView):
+  model = Job
+  form_class = JobForm
+  template_name = 'jobs/job_form.html'
+
+  def form_valid(self, form):
+    form.instance.company = self.request.user.company
+    return super().form_valid(form)
+  
+  def get_success_url(self):
+    return reverse_lazy('jobs:job_detail', kwargs={'pk': self.object.pk})
+
+class JobUpdateView(LoginRequiredMixin, CompanyRequiredMixin, UpdateView):
+  model = Job
+  form_class = JobForm
+  template_name = 'jobs/job_form.html'
+
+  def get_queryset(self):
+    return Job.objects.filter(company=self.request.user.company)
+
+  def get_success_url(self):
+    return reverse_lazy('jobs:job_detail', kwargs={'pk': self.object.pk})
+
+class JobDeleteView(LoginRequiredMixin, CompanyRequiredMixin, DeleteView):
+  model = Job
+  success_url = reverse_lazy('jobs:job_list')
+  template_name = 'jobs/job_confirm_delete.html'
+
+  def get_queryset(self):
+    return Job.objects.filter(company=self.request.user.company)
+
+@login_required
+def reports(request):
+  if not hasattr(request.user, 'company'):
+    raise Http404("Only companies can view reports. :(")
+  
+  return render(request, 'jobs/reports.html')
+
+@login_required
+def apply_to_job(request, pk):
+  job = get_object_or_404(Job, pk=pk)
+
+  if not hasattr(request.user, 'candidate'):
+    raise Http404("Only candidate can apply. :(")
+  
+  candidate = request.user.candidate
+  
+  if request.method == 'POST':
+    form = ApplicationForm(request.POST, candidate=candidate, job=job)
+    if form.is_valid():
+      form.save()
+      return redirect('jobs:job_detail', pk=job.pk)
+  else:
+    form = ApplicationForm(candidate=candidate, job=job, initial={
+      'candidate_last_education': candidate.last_education,
+      'candidate_experience': candidate.experience,
+    })
+
+  return render(request, 'jobs/apply.html', {'form': form, 'job': job})
+
+
+@login_required
+def jobs_per_month(request):
+  qs = (Job.objects
+        .annotate(month=TruncMonth('created_at'))
+        .values('month')
+        .annotate(count=Count('id'))
+        .order_by('month'))
+  labels = [q['month'].strftime('%Y-%m') for q in qs]
+  data = [q['count'] for q in qs]
+  return JsonResponse({'labels': labels, 'datasets': [{'label': 'Jobs per month', 'data': data}]})
+
+
+@login_required
+def apps_per_month(request):
+  qs = (Application.objects
+        .annotate(month=TruncMonth('created_at'))
+        .values('month')
+        .annotate(count=Count('id'))
+        .order_by('month'))
+  labels = [q['month'].strftime('%Y-%m') for q in qs]
+  data = [q['count'] for q in qs]
+  return JsonResponse({'labels': labels, 'datasets': [{'label': 'Applications per Month', 'data': data}]})
