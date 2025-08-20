3caf7c74ab9f6a548c89f7e55fb7ea8d024d1858
Enock <bernardo.enock@vileve.com.br>
2025-08-20 11:55:56 -0300

    add user manager in accounts models

--- accounts/models.py
+++ accounts/models.py
@@ -1,13 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as gl
-
+from .managers import UserManager
 class User(AbstractUser):
   username = None
   email = models.EmailField(gl('email address'), unique=True)
 
   USERNAME_FIELD = 'email'
   REQUIRED_FIELDS = []
+
+
+  objects = UserManager()
+
+  def __str__(self):
+      return self.email
 
 class Company(models.Model):
   user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='company')
